bfac0ac35f415ef6ff2a7930fd55c21385353f57
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-01-09 11:56:44 +0530

    added cors
    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
@@ -22,6 +23,7 @@ dotenv.config({
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
