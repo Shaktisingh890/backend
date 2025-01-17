bbe395b4e02bcd1319174755044fb21d5c410c51
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-01-17 16:52:46 +0530

    update
    update

--- config/firebase.js
+++ config/firebase.js
@@ -7,21 +7,8 @@ let serviceAccount;
 
 if (process.env.NODE_ENV === 'production') {
   // In production, load from Render's secret file or environment variable
-  const secretFilePath = '/etc/secrets/firebase-service-account.json';
+  const serviceAccount = '/etc/secrets/firebase-service-account.json';
 
-  if (fs.existsSync(secretFilePath)) {
-    // Load service account from Render's secret file
-    serviceAccount = JSON.parse(fs.readFileSync(secretFilePath, 'utf8'));
-  } else if (process.env.FIREBASE) {
-    // Decode from Base64-encoded environment variable
-    serviceAccount = JSON.parse(
-      Buffer.from(process.env.FIREBASE, 'base64').toString('utf8')
-    );
-  } else {
-    throw new Error(
-      'Firebase service account configuration is missing in production.'
-    );
-  }
 } else {
   // In development, import the local JSON file using assert { type: 'json' }
   serviceAccount = serviceAccounts;
